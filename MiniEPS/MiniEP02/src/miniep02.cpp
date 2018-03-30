7fadebf5d83019d97bb62204de020e5cdecfb373
Leonardo Lana <leolanavo@gmail.com>
2018-03-30 20:36:27 -0300

    Add last minute modifications to MiniEP02
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- MiniEPS/MiniEP02/src/miniep02.cpp
+++ MiniEPS/MiniEP02/src/miniep02.cpp
@@ -13,18 +13,42 @@ using namespace std;
 
 #define LIMIT 10000
 
-void* deadlock_check(Lake &l) {
+void deadlock_kill() {
+    cout << "Deadlock" << endl;
+    cout << "Pedras: ";
+    print_vector(rocks);
+    exit(0);
+}
+
+void deadlock_check_counter(Lake &l) {
     while (!l.success()) {
-        if (counter > LIMIT) {
-            cout << "Deadlock" << endl;
-            print_vector(rocks);
-            exit(1);
-        }
+        if (counter > LIMIT)
+            deadlock_kill();
+    }
+}
+
+void deadlock_check_vector(Lake &l) {
+    int pos1, pos2, pos3, pos4;
+    while (!l.success()) {
+        pos1 = free_rock + 1 < rocks.size()? free_rock + 1 : free_rock;
+        pos2 = free_rock + 2 < rocks.size()? free_rock + 2 : free_rock;
+
+        pos3 = free_rock - 1 >= 0? free_rock - 1: free_rock;
+        pos4 = free_rock - 2 >= 0? free_rock - 2: free_rock;
+
+        if ((rocks[pos1] == TOAD || rocks[pos1] == FREE) &&
+            (rocks[pos2] == TOAD || rocks[pos2] == FREE) &&
+            (rocks[pos3] == FROG || rocks[pos3] == FREE) &&
+            (rocks[pos4] == FROG || rocks[pos4] == FREE))
+            deadlock_kill();
     }
 }
 
 int main (int argc, char* argv[]) {
-    int n = stoi(argv[1]), m = stoi(argv[2]);
+    int n = stoi(argv[1]);
+    int m = stoi(argv[2]);
+    bool deadlock_check = stoi(argv[3]);
+
     pthread_mutex_init(&mutex, NULL);
 
     Lake l(n, m);
@@ -38,7 +62,10 @@ int main (int argc, char* argv[]) {
         rnd_animals[i].execute_thread(rnd_animals[i].index);
     }
 
-    deadlock_check(l);
+    if (deadlock_check)
+        deadlock_check_counter(l);
+    else
+        deadlock_check_vector(l);
 
     for (int i = 0; i < animals.size(); i++) {
         pthread_join(animals[i].th, NULL);
