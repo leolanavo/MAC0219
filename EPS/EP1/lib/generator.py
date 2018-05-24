78904b73ed87e5a06aea0105c30c4f2aa12d06ef
João Daniel <jotaf.daniel@gmail.com>
2018-05-24 09:39:26 -0300

    Finish test generator
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- EPS/EP1/lib/generator.py
+++ EPS/EP1/lib/generator.py
@@ -1,3 +1,4 @@
+from sys import argv
 import random
 from numpy import random
 import numpy as np
@@ -7,27 +8,46 @@ def randomValue():
     return 0
   return np.random.normal(scale=15)
 
-
-class Matrix:
-  def __init__(self, N, M, randFn=randomValue):
-    self.lines, self.cols = int(N), int(M)
-    self.data = np.array([
-      [randFn() for j in range(self.cols)] 
-          for i in range(self.lines)
-    ])
-
-  def __str__(self):
-    aux = [(i, j, self.data[i][j]) 
-        for i in range(self.lines)
-        for j in range(self.cols)
-        if abs(self.data[i][j]) > 0]
-
-    s = "{} {}\n".format(self.lines, self.cols)
+def printMatrix(m, l, c, file=None):
+  aux = [(i, j, m[i][j]) 
+          for i in range(l) 
+          for j in range(c) 
+          if abs(m[i][j]) > 0]
+ 
+  if file is None:
+    print(l, c)
     for el in aux:
-      s += "{} {} {}\n".format(el[0], el[1], el[2])
+      print(el[0], el[1], el[2])
+  else:
+    file.write("{} {}\n".format(l, c))
+    for el in aux:
+      file.write("{} {} {}\n".format(el[0], el[1], el[2]))
 
-    return s
 
-m = Matrix(10, 20)
+if __name__ == '__main__':
+  M, P, N = [int(float(arg)) for arg in argv[1:4]]
+  fileA, fileB, fileC = argv[4:7]
 
-print(m)
+  ## compute and print
+  a = np.array([[randomValue() for j in range(P)] for i in range(M)])
+
+  with open(fileA, 'w+') as f:
+    printMatrix(a, M, P, f)
+  f.closed
+
+
+  ## compute and print
+  b = np.array([[randomValue() for j in range(N)] for i in range(P)])
+
+  with open(fileB, 'w+') as f:
+    printMatrix(b, P, N, f)
+  f.closed
+
+
+  ## compute and print
+  c = np.matmul(a, b)
+
+  with open(fileC, 'w+') as f:
+    printMatrix(c, M, N, f)
+  f.closed
+
